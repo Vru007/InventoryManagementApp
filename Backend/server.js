6ad0bd66f01ae23117285ab093d50c99e902fa22
Vru007 <vrushik7143work@gmail.com>
2023-08-30 01:05:41 +0530

    Restart

--- Backend/server.js
+++ Backend/server.js
@@ -1,35 +1,22 @@
-const dotenv = require("dotenv").config();
-const express= require("express");
+const dotenv = require("dotenv").config(); // Invoke the config function
+const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser= require("body-parser");
-const cors=require("cors");
+const bodyParser = require("body-parser"); // Corrected typo
+const cors = require("cors");
 
-const app=express();
-// Middlewares
+const app = express();
+const PORT = process.env.PORT || 5000; // Corrected case of 'PORT'
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-
-
-// Routes
-app.get("/",(req,res)=>{
-      res.send("Home-Page")
-});
-
-const PORT= process.env.PORT || 5000;
-
-
-//connect to mongoDB and starting server;
-
-
+// connect with mongoose
+mongoose.set('strictQuery', false);
 mongoose
-      .connect(process.env.MONGO_URI)
-      .then(()=>{
-            app.listen(PORT,()=>{
-                  
-                  console.log(`server Running on port ${PORT}`)
-            })
-      })
-      .catch((err)=>console.log(err))
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
---
+++ front/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home/Home";
+
+// axios.defaults.withCredentials = true;
+
+ function App() {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     async function loginStatus() {
+//       const status = await getLoginStatus();
+//       dispatch(SET_LOGIN(status));
+//     }
+//     loginStatus();
+//   }, [dispatch]);
+
+  return (
+    <BrowserRouter>
+     
+    <h1>Hello Front</h1>
+       
+    </BrowserRouter>
+  );
+}
+
+export default App;
